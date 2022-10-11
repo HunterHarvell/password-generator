91f65c9a526b5e2275c0018d084489a1ecfd6374
HunterHarvell <hunterharvell95@gmail.com>
2022-10-11 10:36:48 -0500

    adds files from student repo

---
+++ assets/script.js
@@ -0,0 +1,46 @@
+var playAgain;
+var userInput = "";
+var cpuInput = "";
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+// constant
+var OPTIONS = ["R", "P", "S"];
+
+
+// What are the steps?
+
+/*
+
+1. prompt the user for their choice
+2. user makes their choice
+3. validate choice
+4. store userInput in state
+5. computer make choice
+6. alerted user of cpu choice
+7. compare choices
+    -if user R
+        -if cpu picks "R" then tie
+
+
+
+
+*/
+
+if(userInput === "R") {
+    if(cpuInput  === "R") {
+        console.log(tie);
+    }
+} else if(userInput === "P") {
+
+}else if (userInput === "S") {
+    
+} else {
+    console.log("incoorect input");
+}
+
+
+var input = prompt("jkfljdskaljfds");
+var playAgain = confirm("kjldajkls");
+alert("kldjaklsd");
