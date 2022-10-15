f90575e89faf15757ba40ba2d6fe3560608a2295
HunterHarvell <hunterharvell95@gmail.com>
2022-10-15 15:12:09 -0500

    adds JS for user prompts and generating characters for PW

--- assets/script.js
+++ assets/script.js
@@ -5,12 +5,55 @@ const numerals = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 const specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "~", "`", ";", ":", "?", "/", "<", ">"];
 
 
+
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // Sets variables and array to hold password information
+  var passwordInfo = "";
+  const passChar = [];
+
+  // Password Integers
+  var characterAmount = window.prompt("Enter the amount of characters you want for your password. NOTE: Must be between 8-128 characters");
+  if (characterAmount >= 8 && characterAmount <=128) {
+    var getNumeral = window.confirm("Would you like to include numbers in your password? (OK for 'YES' & CANCEL for 'NO')");
+    if (getNumeral) {
+      passwordInfo += numerals;
+      passChar.push(Math.floor(Math.random()*numerals.length));
+    };
+
+    // Would you like special characters?
+    var getSC = window.confirm("Would you like to use special characters in your password? (OK for 'YES' & CANCEL for 'NO')");
+    if (getSC) {
+      passwordInfo += specialChar;
+      passChar.push(Math.floor(Math.random()*specialChar.length));
+    };
+
+    // Would you like lower case?
+    var getLC = window.confirm("Would you like to use lower case letters in your password? (OK for 'YES' & CANCEL for 'NO')");
+    if (getLC) {
+      passwordInfo += letterLower;
+      passChar.push(Math.floor(Math.random()*letterLower.length));
+    };
+
+    // Would you like upper case?
+    var getUC = window.confirm("Would you like to use upper case letters in your password? (OK for 'YES' & CANCEL for 'NO')");
+    if (getUC) {
+      passwordInfo += letterUpper;
+      passChar.push(Math.floor(Math.random()*letterUpper.length));
+    };
+
+    // notify user needs to select at least one option
+    if (!passwordInfo) {
+      window.alert("You need to select at least one option, please try again!");
+    };
+  };
+
+  var password = "";
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
